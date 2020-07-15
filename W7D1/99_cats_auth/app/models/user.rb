10af16355f127b74b2061da7b0154ff8a253f196
thewizard23 <seanpscott6@gmail.com>
2020-07-15 08:45:14 -0700

    music project

--- W7D1/99_cats_auth/app/models/user.rb
+++ W7D1/99_cats_auth/app/models/user.rb
@@ -2,6 +2,8 @@ class Users < ApplicationRecord
     validates :user_name, presence:true, uniqueness:true
     validates :password_digest, presence:true
     validates :session_token, presence:true, uniqueness:true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    attr_reader :password
 
     after_initialize :ensure_session_token
 
